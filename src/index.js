1559c6d5d7f987e97400e9a576e2857eb281e251
Megan Clarke <38575051+mclarkedev@users.noreply.github.com>
2019-01-17 20:12:28 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import otherSquare, { add, name } from './utilities'
-import otherScream  from './scream'
 
-console.log(otherScream(name))
-console.log('index.js')
-console.log(add(32, 3))
-console.log(otherSquare(10))
