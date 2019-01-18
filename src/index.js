5e265d517493b964fd426c3a3628bd38283b6ac8
Megan Clarke <meganclarkedev@gmail.com>
2019-01-17 19:59:03 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import otherSquare, { add, name } from './utilities'
+import otherScream  from './scream'
+
+console.log(otherScream(name))
+console.log('index.js')
+console.log(add(32, 3))
+console.log(otherSquare(10))
